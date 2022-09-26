0abc645c33228591d8b01acf51825af035af09ca
CynthiaMugo <cynthiamugo25@gmail.com>
2022-09-26 10:54:49 -0700

    python list using C

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,20 @@
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
+
+/**
+ * print_python_list_info -> prints some basic info abt python list
+ * @p: python object
+ */
+
+void print_python_list_info(PyObject *p);
+{
+	long int sixe = Pylist_Size(p);
+	int i;
+	PylistObject *obj = (PylistObject *p);
+
+	printf("[*] Size of the Python List = %li\n", size);
+	printf("[*] Allocated = %li\n", obj->allocated);
+	for (i = 0; i < size; i++)
+		printf("Element %i: %s\n", i, Py_Type(obj->ob_item[i])->tp_name);
+}
