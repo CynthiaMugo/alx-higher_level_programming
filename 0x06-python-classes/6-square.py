54c2f12dab78f68dc4e77eb5c7076682b48881fa
CynthiaMugo <cynthiamugo25@gmail.com>
2022-10-12 12:19:39 -0700

    position hot fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -53,6 +53,6 @@ class Square:
 
         [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__size[0])]
+            [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
             print("")
