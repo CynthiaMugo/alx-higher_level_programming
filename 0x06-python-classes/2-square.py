6bf3bd87f297bea15ade46490222eebfe44e11cb
CynthiaMugo <cynthiamugo25@gmail.com>
2022-10-12 11:22:14 -0700

    defines a square

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module that defines a square"""
+
+class Square:
+    """A class that reps a square"""
+
+    def __init__(self, size=0):
+        """Initializing this square class
+        Args:
+            size: reps the sixe of the square defined
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less than zero
+        """
+
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
