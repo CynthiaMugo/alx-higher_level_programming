eda4ec22209598c2586a245abaeb9da2a794f68e
CynthiaMugo <cynthiamugo25@gmail.com>
2022-10-12 11:07:30 -0700

    empty class square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Define a class square"""
+
+class Square:
+    """Represent a square"""
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+This is a file containing solutions to python classes and objects tasks.
