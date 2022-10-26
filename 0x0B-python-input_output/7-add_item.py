59ca7c903adec19ffa53f19f2c223eeadb1b7262
CynthiaMugo <cynthiamugo25@gmail.com>
2022-10-26 05:38:03 -0700

    adds all arguments to a Python list

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
             __import__('6-load_from_json_file').load_from_json_file
 
     try:
-        items = load_from_json_file("add_items.json")
+        items = load_from_json_file("add_item.json")
     except FileNotFoundError:
         items = []
     items.extend(sys.argv[1:])
