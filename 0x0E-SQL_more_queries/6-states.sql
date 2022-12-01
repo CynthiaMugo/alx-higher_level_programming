eb5a26113250ef317e2e1a76ca7eea8ef973931b
CynthiaMugo <cynthiamugo25@gmail.com>
2022-12-01 12:06:25 +0000

    usa states

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -5,6 +5,6 @@
 -- If the database hbtn_0d_usa already exists, your script should not fail
 -- If the table states already exists, your script should not fail
 
-CREATE DATABSE IF NOT EXISTS hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 name VARCHAR(256) NOT NULL);
