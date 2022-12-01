0d829c1297a538f36c385f169ddf33dc8eaec97e
CynthiaMugo <cynthiamugo25@gmail.com>
2022-12-01 12:34:12 +0000

    genre id

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -8,4 +8,4 @@ SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 INNER JOIN tv_shows_genres
 ON tv_shows.id=tv_shows_genres.show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
