9837a60a55e9f6281fcd7d2208cda93773647572
CynthiaMugo <cynthiamugo25@gmail.com>
2022-12-01 13:03:43 +0000

    fix

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -9,4 +9,4 @@ FROM tv_shows
 LEFT JOIN tv_shows_genres
 ON tv_shows.id=tv_show_genres.show_id
 WHERE tv_show_genres.show_id IS NULL
-ORDER BY tv_shows.title, tv_show_genres.genre.id;
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
